4f411ca3ac65589cdc7797125e920ba0ea71c264
Aland Mariwan <alandmariwan@gssd.de>
2023-01-06 10:39:56 +0100

    'fix'

--- assets/js/cltserver.js
+++ assets/js/cltserver.js
@@ -1,4 +1,9 @@
 // This function can lead to errors
+
+const url = {
+	main: 'https://192.168.44.185:4000/',
+	sub: 'https://192.168.44.185:4000/sub',
+};
 connectToServer = async (url, type, async, body) => {
 	console.log(url, type, async, body);
 	return new Promise((resolve, reject) => {
